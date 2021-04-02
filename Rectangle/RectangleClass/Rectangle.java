777bb268987d9c6e9a568b84801940b4e6f567e8
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-04-02 19:24:24 +0900

    Create Rectangle.java

---
+++ Rectangle/RectangleClass/Rectangle.java
@@ -0,0 +1,11 @@
+package Rectangle.RectangleClass;
+
+public class Rectangle {
+    public double base;
+    public double height;
+
+    public double getArea() {
+        double area = base * height;
+        return area;
+    }
+}
