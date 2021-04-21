40775380948c0b1deb70aae63db50d4aade0af59
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-04-21 22:57:37 +0900

    Create Circle.java

---
+++ CircleClass/Circle.java
@@ -0,0 +1,10 @@
+package CircleClass;
+
+public class Circle {
+    public double radius;
+
+    public double getArea() {
+        double area = 3.14 * radius * radius;
+        return area;
+    }
+}
