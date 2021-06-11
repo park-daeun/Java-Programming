808d7464e90772c8f3bfac6658d05c11a3a1bd05
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-06-12 00:30:13 +0900

    Create Q2_ThreeButton.java

---
+++ Week12/Q2_ThreeButton.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+class ContentPaneEx extends JFrame {
+    public ContentPaneEx() {
+        setTitle("300x300 Swing Frame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = getContentPane();
+        contentPane.setBackground(Color.ORANGE);
+        contentPane.setLayout(new FlowLayout());
+
+        contentPane.add(new JButton("OK"));
+        contentPane.add(new JButton("Cancel"));
+        contentPane.add(new JButton("Ignore"));
+
+        setSize(300, 150);
+        setVisible(true);
+    }
+}
+
+public class Q2_ThreeButton {
+    public static void main(String[] args) {
+        new ContentPaneEx();
+    }
+}
